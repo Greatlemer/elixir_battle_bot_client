e3726e5928a4fc663fcff6ee6f9162a12a8f3f49
Greatlemer <coder@Adrian-Rogerss-MacBook-Pro.local>
2017-07-10 00:08:00 +0100

    Don't actually print stuff at the moment.

--- lib/NoughtsAndCrosses/strategy_plan.ex
+++ lib/NoughtsAndCrosses/strategy_plan.ex
@@ -4,6 +4,7 @@ defmodule NoughtsAndCrosses.StrategyPlan do
   # * Then flip the board horizontally and also apply the strategy to the four rotations of that
   # * Then remove any duplicates (keep the first example of each)
   defmacro strategy(_as_string, "when the board represents:", board, _move) do
-    IO.puts board
+    # IO.puts board
+    board
   end
 end
